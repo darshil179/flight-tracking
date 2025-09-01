5a95da0f255d584be74fa5719dcf0261b273520a
darshilbhakhar179@gmail.com <darshilbhakhar179@gmail.com>
2025-08-31 22:19:27 -0400

    intial commit setup

---
+++ src/main/java/com/example/flighttracking/FlighttrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.flighttracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighttrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighttrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flighttracking/FlighttrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flighttracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlighttrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
